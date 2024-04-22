2aec6584e34b4052b965796d8aea74806c3c9f26
nagarjunags <nagarjunags2014@gmail.com>
2024-04-22 12:49:31 +0530

    added dark and light theme,

--- web/readJSON.js
+++ web/readJSON.js
@@ -139,3 +139,27 @@ document.getElementById("run-script-btn").addEventListener("click", () => {
       console.error("Error:", error);
     });
 });
+
+// Function to toggle between dark and light themes
+// Function to toggle between dark and light themes
+function toggleTheme() {
+  const body = document.body;
+  const container = document.querySelector(".container"); // Select the .container div
+  body.classList.toggle("dark-theme");
+  container.classList.toggle("dark-theme");
+
+  // Get the theme button
+  const themeButton = document.getElementById("theme-btn");
+
+  // Check the current text of the button and toggle it
+  if (themeButton.innerHTML == "Dark Theme") {
+    themeButton.innerHTML = "Light Theme";
+  } else {
+    themeButton.innerHTML = "Dark Theme";
+  }
+}
+
+// Add event listener to the theme button
+document.getElementById("theme-btn").addEventListener("click", toggleTheme);
+
+// Add event listener to the light theme button
